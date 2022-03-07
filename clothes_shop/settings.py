e137e5c6d9e2cbe0852d6b0ac4ab26082c2f6a1b
Joanna Gorska <gorska919@gmail.com>
2022-03-07 13:34:20 +0000

    Add settings to connect static files

--- clothes_shop/settings.py
+++ clothes_shop/settings.py
@@ -159,3 +159,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
