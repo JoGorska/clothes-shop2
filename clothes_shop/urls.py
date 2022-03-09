e108a53812f935645674e6b9d8aca6ce00ea937f
Joanna Gorska <gorska919@gmail.com>
2022-03-09 22:45:53 +0000

    Add product.html tepmpale.

--- clothes_shop/urls.py
+++ clothes_shop/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('products/', include('product.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
