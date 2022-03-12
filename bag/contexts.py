4eecb9dccee8d3327d176e008e54ac6feeba8a5e
Joanna Gorska <gorska919@gmail.com>
2022-03-12 19:46:38 +0000

    Change context.py so it forloops through the items in the bag to get details of each items

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,15 +1,32 @@
 from decimal import Decimal
 from django.conf import settings
 
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def bag_contents(request):
     '''
      context processor, make this dictionary available to all templates across
      the entire application
     '''
+    # bag_items is a list of dictionaries from a for loop below
     bag_items = []
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    # forloops all items in the bag gets the product, quantity and price
+    # appends the list bag_items with the dictionary of item id, quantity and product name
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # Decimal is financial function, it would not lead to
