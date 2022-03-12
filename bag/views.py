4e57f150422e037b9f559d624c0d427023064a5a
Joanna Gorska <gorska919@gmail.com>
2022-03-12 15:35:49 +0000

    Add bag temlate and view to render it

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_bag(request):
+    """
+    A view that renders a bag content page
+    """
+    return render(request, 'bag/bag.html')
