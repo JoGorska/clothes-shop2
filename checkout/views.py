1173b12c5a7806dd7035e406c03b0034f661a060
Joanna Gorska <gorska919@gmail.com>
2022-03-16 22:31:03 +0000

    Add views and settings for stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,15 @@
+import os
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-import os
+
 if os.path.isfile("env.py"):
     import env
-
+from django.conf import settings
 from .forms import OrderForm
+from bag.context import bag_contents
+
+import stripe
+
 
 def checkout(request):
     '''
@@ -14,6 +19,10 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    # stripe requires the payment to be passed as integer
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     stripe_public_key = os.environ.get('stripe_public_key')
