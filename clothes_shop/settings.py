1c88ae9ee635611ed7d79103b51fd97f92f23917
Joanna Gorska <gorska919@gmail.com>
2022-03-16 15:40:23 +0000

    Add crispy forms and update settings and add folder structure needed for future css file

--- clothes_shop/settings.py
+++ clothes_shop/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # other apps
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'clothes_shop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
