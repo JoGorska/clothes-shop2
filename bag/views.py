a3e6d05e29807cad74f79584bd412ad60c5777fa
Joanna Gorska <gorska919@gmail.com>
2022-03-16 12:27:04 +0000

    Add settings and updated views and base template to display messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -15,6 +17,7 @@ def add_to_bag(request, item_id):
     Add quantity of the specified product to the shopping bag
     """
     # it will come from template as a string - need to convert to integer
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -37,6 +40,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
