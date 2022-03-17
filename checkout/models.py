2c3902fd97ebe2183ce2757dea95c60a9c31ca34
Joanna Gorska <gorska919@gmail.com>
2022-03-17 23:01:02 +0000

    Add post method to checkout view and checkout_success view

--- checkout/models.py
+++ checkout/models.py
@@ -38,7 +38,9 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        # or 0 is to prevent setting order_total to none if we manually
+        # delete all orders from the database
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
