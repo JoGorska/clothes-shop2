fe2109aca2b356b7e91f1a80ae3141eecba5845f
Joanna Gorska <gorska919@gmail.com>
2022-03-18 15:35:49 +0000

    Add view to handle if user wants to save information of the order

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -1,12 +1,11 @@
 from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
-from django.views.decorators.csrf import csrf_exempt 
+from django.views.decorators.csrf import csrf_exempt
 
 from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
-
 
 @require_POST
 @csrf_exempt
@@ -34,4 +33,23 @@ def webhook(request):
     except Exception as e:
         return HttpResponse(content=e, status=400)
 
-    return HttpResponse(status=200)
+    # Set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # Map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # Get the webhook type from Stripe, this pulls from stripe exact type
+    # of event
+    event_type = event['type']
+
+    # If there's a handler for it, get it from the event map
+    # Use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # Call the event handler with the event
+    response = event_handler(event)
+    return response
