ddabbec653129b5a45ee8fd68046fb4b501a45f0
Joanna Gorska <gorska919@gmail.com>
2022-03-20 11:48:32 +0000

    Update allauth templates to match styling of the page

--- clothes_shop/urls.py
+++ clothes_shop/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
