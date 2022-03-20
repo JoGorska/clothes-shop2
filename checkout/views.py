c13aeec05169b69eec65361392dc9b1da3c9f3a0
Joanna Gorska <gorska919@gmail.com>
2022-03-20 17:18:27 +0000

    Add user profile information to order form

--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from django.contrib import messages
 from django.conf import settings
 
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -115,8 +117,27 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-        order_form = OrderForm()
+        # pre fills the form with initial values if the user
+        # is authenticated. the data comes from user profile
+        # what if no data?
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -138,14 +159,14 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-
+    # manage profile info only if user is authenticated
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
         # Attach the user's profile to the order
         order.user_profile = profile
         order.save()
 
-        # Save the user's info
+        # Save the user's info to the profile
         if save_info:
             profile_data = {
                 'default_phone_number': order.phone_number,
