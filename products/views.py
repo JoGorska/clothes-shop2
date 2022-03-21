d395a5d8c89c0e6c1206af261e84814f58f8416f
Joanna Gorska <gorska919@gmail.com>
2022-03-21 19:54:21 +0000

    Minor fixes of urls in bag

--- products/views.py
+++ products/views.py
@@ -82,7 +82,7 @@ def add_product(request):
             messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
