a0d1a81feedebca2dbfeeda170834de4b42ca66a
Joanna Gorska <gorska919@gmail.com>
2022-03-22 07:52:20 +0000

    Add postgres database to settings

--- clothes_shop/settings.py
+++ clothes_shop/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile("env.py"):
     import env
 
@@ -127,12 +128,17 @@ WSGI_APPLICATION = 'clothes_shop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # sqlite database ??? when attach postgresql???
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
